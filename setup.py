52d49ef63fed029bcf349f7d96929d0d31995d6a
Leonard Pollak <leonardp@tr-host.de>
2022-05-21 16:42:36 +0200

    really

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     author_email="pollak@tr-host.de",
     description="python interface for AlexeyAB/darknet",
     # Modules to import from other scripts:
-    package_dir = {'darknet': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src')
     # Executables
     scripts=["cli/darknet_images.py"],
 )
